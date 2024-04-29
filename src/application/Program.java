dee275a906d7fdf813641a6005be25541e2f7355
Wall <Wallaceknn@gmail.com>
2024-04-29 03:13:25 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
-
+        Seller seller = sellerDao.findById(1);
         System.out.println(seller);
 
     }
